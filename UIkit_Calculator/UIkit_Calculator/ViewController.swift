db5103c671cbbef5c5e71e4acbafc84e774cfd2f
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-05-16 22:34:13 +0800

    Add files via upload

---
+++ UIkit_Calculator/UIkit_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkit_Calculator
+//
+//  Created by fanqing_m on 2021/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIkit_Calculator/UIkit_CalculatorTests/UIkit_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  UIkit_CalculatorTests.swift
+//  UIkit_CalculatorTests
+//
+//  Created by fanqing_m on 2021/5/16.
+//
+
+import XCTest
+@testable import UIkit_Calculator
+
+class UIkit_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
