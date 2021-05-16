c0011291b2eb1f2d1385a53cd883af877fd2be15
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-05-16 22:03:35 +0800

    Update ContentView.swift

--- SwiftUI_Calculator/Calculator/ContentView.swift
+++ SwiftUI_Calculator/Calculator/ContentView.swift
@@ -55,6 +55,7 @@ struct ContentView: View {
     @State var num1 = 0
     @State var num2 = 0
     @State var temp = 0        //储存运算的中间结果
+    @State var flag1 = true    //我们从根源上不让用户输入错误，即输入1+后不能再输入-这样的操作服了。得闲输入数字
     
     let buttons: [[CalcButton]] = [
         [.clear, .negative, .percent, .divide],
@@ -112,7 +113,7 @@ struct ContentView: View {
         case .add: return 0
         case .subtract: return 1
         case .multiply: return 2
-        case .divide: return 3
+        case .divide: return 2
         case .equal: return -1
         case .none:
             return -2
@@ -137,39 +138,44 @@ struct ContentView: View {
         return Int(self.value) ?? 0
     }
     func didTap(button: CalcButton) {
+        var flag = true
         switch button {
         case .add, .subtract, .multiply, .divide, .equal:
-            if button == .add {
+            if button == .add && self.flag1{
 //                self.currentOperation = .add
                 self.runningNumber = Int(self.value) ?? 0
                 self.numbers.push(element: runningNumber)
                 self.userValue += "+"
                 self.options.push(element: .add)
                 self.length2+=1
+                self.flag1 = false
             }
-            else if button == .subtract {
+            else if button == .subtract && self.flag1 {
 //                self.currentOperation = .subtract
                 self.runningNumber = Int(self.value) ?? 0
                 self.numbers.push(element: runningNumber)
                 self.userValue += "-"
                 self.options.push(element: .subtract)
                 self.length2+=1
+                self.flag1 = false
             }
-            else if button == .multiply {
+            else if button == .multiply && self.flag1{
 //                self.currentOperation = .multiply
                 self.runningNumber = Int(self.value) ?? 0
                 self.numbers.push(element: runningNumber)
                 self.userValue += "x"
                 self.options.push(element: .multiply)
                 self.length2+=1
+                self.flag1 = false
             }
-            else if button == .divide {
+            else if button == .divide && self.flag1{
 //                self.currentOperation = .divide
                 self.runningNumber = Int(self.value) ?? 0
                 self.numbers.push(element: runningNumber)
                 self.userValue += "÷"
                 self.options.push(element: .divide)
                 self.length2+=1
+                self.flag1 = false
             }
             //根据操作数栈和操作符号栈的情况计算结果
             else if button == .equal {
@@ -177,80 +183,105 @@ struct ContentView: View {
                 self.userValue += "="
                 var a = self.numbers.count
                 var b = self.options.count
+                print("numbers \(a)  options \(b)")
 //                let runningValue = self.runningNumber
 //                let currentValue = Int(self.value) ?? 0
 //                self.Calculate(num1: runningNumber, num2: currentValue, op: currentOperation)
                 //注意num1,num2和进栈顺序是反的
                 //下面我们根据操作数栈和操作符号栈里面的东西算结果
                 //把等号也加进去了
-                while a>=2 && b>=2
+                if a<2
                 {
-                    let num1 = self.numbers.pop() ?? 0
-                    
-                    let op = self.options.pop() ?? .none
-                    if(b>1)
+                    self.userValue = "error"
+                }
+                else
+                {
+                    while a>=2 && b>=2 && flag
                     {
-                        if(self.Prioity(op: op) > self.Prioity(op: self.options.top() ?? .none))
+                        let num1 = self.numbers.pop() ?? 0
+                        
+                        let op = self.options.pop() ?? .none
+                        if(b>1)
+                        {
+                            if(self.Prioity(op: op) > self.Prioity(op: self.options.top() ?? .none))
+                            {
+                                let num2 = self.numbers.pop() ?? 0
+                                temp = self.Calculate(num1: num1, num2: num2, op: op)
+                                self.numbers.push(element: temp)
+                                a = self.numbers.count
+                                b = self.options.count
+                            }
+                            else
+                            {
+                                var numbers2 = Stack<Int>()
+                                numbers.push(element: num1)
+                                var options2 = Stack<Operation>()
+                                options2.push(element: op)
+                                //延迟运算
+                                //这下面也不能直接做，因为还可能延迟运算
+                                while b>=2
+                                {
+                                    let op2 = self.options.pop() ?? .none
+                                    if self.numbers.isEmpty()
+                                    {
+                                        self.userValue = "error"
+                                        flag = false
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        let num2 = self.numbers.pop() ?? 0
+                                        options2.push(element: op2)
+                                        numbers2.push(element: num2)
+                                        if self.Prioity(op: op2) > self.Prioity(op: self.options.top() ?? .none)
+                                        {
+                                            options2.pop()
+                                            numbers2.pop()
+                                            self.numbers.push(element: num2)
+                                            self.options.push(element: op2)
+                                            break;
+                                        }
+                                    }
+                                }
+                                if self.numbers.count < 2
+                                {
+                                    self.userValue = "error"
+                                    break;
+                                }
+                                else
+                                {
+                                    let num4 = self.numbers.pop() ?? 0
+                                    let num5 = self.numbers.pop() ?? 0
+                                    let op3 = self.options.pop() ?? .none
+                                    temp = self.Calculate(num1: num4, num2: num5, op: op3)
+                                    self.numbers.push(element: temp)
+                                    
+                                    var cnt1 = numbers2.count
+                                    var cnt2 = options2.count
+                                    while cnt1>0
+                                    {
+                                        let s = numbers2.pop() ?? 0
+                                        self.numbers.push(element: s)
+                                        cnt1-=1
+                                    }
+                                    while cnt2>0
+                                    {
+                                        let m = options2.pop() ?? .none
+                                        self.options.push(element: m)
+                                        cnt2-=1
+                                    }
+                                }
+                            }
+                        }
+                        else
                         {
                             let num2 = self.numbers.pop() ?? 0
+                            //说明操作符栈中已经没东西了
                             temp = self.Calculate(num1: num1, num2: num2, op: op)
                             self.numbers.push(element: temp)
                             a = self.numbers.count
                             b = self.options.count
                         }
-                        else
-                        {
-                            var numbers2 = Stack<Int>()
-                            numbers.push(element: num1)
-                            var options2 = Stack<Operation>()
-                            options2.push(element: op)
-                            //延迟运算
-                            //这下面也不能直接做，因为还可能延迟运算
-                            while b>=2
-                            {
-                                let op2 = self.options.pop() ?? .none
-                                let num2 = self.numbers.pop() ?? 0
-                                options2.push(element: op2)
-                                numbers2.push(element: num2)
-                                if self.Prioity(op: op2) > self.Prioity(op: self.options.top() ?? .none)
-                                {
-                                    options2.pop()
-                                    numbers2.pop()
-                                    self.numbers.push(element: num2)
-                                    self.options.push(element: op2)
-                                    break;
-                                }
-                            }
-                            let num4 = self.numbers.pop() ?? 0
-                            let num5 = self.numbers.pop() ?? 0
-                            let op3 = self.options.pop() ?? .none
-                            temp = self.Calculate(num1: num4, num2: num5, op: op3)
-                            self.numbers.push(element: temp)
-                            
-                            var cnt1 = numbers2.count
-                            var cnt2 = options2.count
-                            while cnt1>0
-                            {
-                                let s = numbers2.pop() ?? 0
-                                self.numbers.push(element: s)
-                                cnt1-=1
-                            }
-                            while cnt2>0
-                            {
-                                let m = options2.pop() ?? .none
-                                self.options.push(element: m)
-                                cnt2-=1
-                            }
-                        }
-                    }
-                    else
-                    {
-                        let num2 = self.numbers.pop() ?? 0
-                        //说明操作符栈中已经没东西了
-                        temp = self.Calculate(num1: num1, num2: num2, op: op)
-                        self.numbers.push(element: temp)
-                        a = self.numbers.count
-                        b = self.options.count
                     }
                 }
                     //self.userValue+=self.value
@@ -266,10 +297,14 @@ struct ContentView: View {
         case .clear:
             self.userValue = "0"
             self.value = "0"
+            self.flag1 = true
+            self.numbers.clear()
+            self.options.clear()
         //暂时没有完成这些功能
         case .negative, .percent:
             break
         default:
+            self.flag1 = true
             let number = button.rawValue
             if self.userValue == "0" {
                 self.value = number
@@ -306,4 +341,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
